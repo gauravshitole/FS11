5e92d8988dbfb0bb26c85a6582164b6c1da078a9
gaurav shitole <103493709+gauravshitole@users.noreply.github.com>
2023-11-30 11:22:12 +0530

    Add files via upload

---
+++ LoginForm.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+
+const LoginForm = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // Simple validation for username and password
+    if (username === 'Admin' && password === 'Pass123') {
+      console.log('Login successful!');
+    } else {
+      console.log('Unauthorized. Please check your credentials.');
+    }
+  };
+
+  return (
+    <div>
+        <h1>Slip 11</h1>
+      <h2>Login Form</h2>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleLogin();
+        }}
+      >
+        <label>
+          Username:
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginForm;
